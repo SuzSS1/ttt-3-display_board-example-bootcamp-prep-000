e8e2ee145875ffdbd77dd585f1e665f8b369a699
istrage1621@gmail.com <istrage1621@gmail.com>
2018-03-24 03:45:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-display_board = "Tic Tac Toe"
+def display_board
+  cell = "   "
+  row = " | | "
+  puts cell
+  puts cell
+  puts cell
+  dash = -----------
+  puts dash
+  
